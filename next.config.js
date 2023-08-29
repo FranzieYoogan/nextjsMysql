bf916943c03e555f1d2375aa7ccd67bce70e5345
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-08-29 00:44:31 -0300

    connectingMysql

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
 
+    
+}
+
+const mysql = require("mysql");
+
+const conexao = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "gamesblog"
+});
+console.log('Connected to MySQL database!');
+
+module.exports = conexao;
 module.exports = nextConfig
